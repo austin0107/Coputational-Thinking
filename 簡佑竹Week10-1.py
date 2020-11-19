37aa3f01cd7f1eafedf24677745d21bf86fb89f3
austin0107 <71420952+austin0107@users.noreply.github.com>
2020-11-19 11:19:28 +0800

    Add files via upload

---
+++ 簡佑竹Week10-1.py
@@ -0,0 +1,28 @@
+#  寫程式是怎麼一回事
+
+## Question: [魔球]那部電影值不值得看? 老闆想了解一下那部電影的相關資訊
+
+# Step1 : 上網找 哪裡找 找什麼 如何回覆
+## 從IMDb電影資料庫找出要記錄的魔球電影相關資訊
+## 網址 : https://www.imdb.com/title/tt1210166/?ref_=fn_al_tt_1
+## 相關資訊 : 電影名稱 、電影上映年份 、電影時間長度 、電影的導演 、電影的主角
+
+# Step2 : Input-Process-Output
+
+# Step3 : Coding
+
+movie_title = "money_ball"
+release_year = "2011"
+movie_time_mins = 133
+movie_rating = 7.6
+
+director = "\n Bennett Miller"
+stars_1 = "\n Brad Pitt"
+stars_2 = "\n Robin Wright"
+stars_3 = "\n Jonah Hill"
+
+print("電影英文名稱",movie_title)
+print("電影上映年份",release_year)
+print("電影時間長度",movie_time_mins)
+print("電影的導演",director)
+print("電影主角",stars_1,stars_2,stars_3)
