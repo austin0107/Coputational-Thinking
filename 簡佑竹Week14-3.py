f8eb416d5066b505fe2e28bfb871c8e2dda42930
austin0107 <71420952+austin0107@users.noreply.github.com>
2020-12-21 21:15:21 +0800

    Add files via upload

---
+++ 簡佑竹Week14-3.py
@@ -0,0 +1,13 @@
+# 作業
+import turtle
+turtle.colormode(255)
+austin = turtle.Turtle()
+austin.color(255, 215, 0)
+austin.pensize(5)
+austin.penup()
+austin.backward(175)
+austin.pendown()
+
+for i in range(5):
+    austin.forward(300)
+    austin.right(144)
