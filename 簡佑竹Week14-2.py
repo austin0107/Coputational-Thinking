a8473c87bfc6b3b9bcc46000c9d515365f26dc4f
austin0107 <71420952+austin0107@users.noreply.github.com>
2020-12-21 20:39:53 +0800

    Update 簡佑竹Week14-2.py

--- 簡佑竹Week14-2.py
+++ 簡佑竹Week14-2.py
@@ -18,14 +18,3 @@ shu.circle(250,180,30)
 shu.end_fill()
 shu.stamp()
 shu.write('Turtle Rock')
-
-
-import turtle
-turtle.colormode(250)
-pen = turtle.Turtle()
-pen.color = (255,215,0)
-pen.pensize(10)
-
-for a in range(5):
-    pen.forward(250)
-    pen.right(144)
